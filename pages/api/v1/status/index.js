f4e3141f28fd2ab69cc323a0eae3271a28fd6e3c
Marcus Santos <103291678+MarquinDj@users.noreply.github.com>
2025-11-03 23:31:56 +0000

    finaliza o endpoint  `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Gamer Funcionando" });
+  const updatedAt = new Date().toISOString();
+  const pgVersionResult = await database.query("SHOW server_version;");
+  const pgVersionValue = pgVersionResult.rows[0].server_version;
+  const pgMaxConResult = await database.query("SHOW max_connections");
+  const pgMaxConValue = pgMaxConResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConValue = openedConResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      version: pgVersionValue,
+      max_connections: parseInt(pgMaxConValue),
+      opened_connections: openedConValue,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const queryVersion = responseBody.dependencies.version;
+  expect(queryVersion).toEqual("16.0");
+
+  const maxCon = responseBody.dependencies.max_connections;
+  expect(maxCon).toEqual(100);
+
+  const openedCon = responseBody.dependencies.opened_connections;
+  expect(openedCon).toEqual(1);
 });
